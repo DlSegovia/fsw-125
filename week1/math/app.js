6b6d5ca8a03dbcc5c82816ffed4161450dd543b5
DlSegovia <daniel.segovia@bryanuniversity.edu>
2020-06-05 15:31:39 -0500

    assignment 1 node module basics and patterns

---
+++ week1/math/app.js
@@ -0,0 +1,11 @@
+const {sum, subtract, multiply, divide} = require("./math.js")
+
+// const mult = require('./math2.js')
+
+console.log(sum(20, 10))
+
+console.log(subtract(20, 10))
+
+console.log(multiply(10, 2))
+
+console.log(divide(20, 10))
