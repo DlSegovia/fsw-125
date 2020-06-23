095b3fef6b43be94371f93e65b8251aff8e883b4
DlSegovia <daniel.segovia@bryanuniversity.edu>
2020-06-22 22:18:16 -0500

    assignment express router

---
+++ week3/p_and_p_express_router/server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+app.use("/bounties", require("./routes/bountyRouter.js"))
+
+app.listen(5000, () => {
+    console.log("Server is listening on port: 5000");
+});
